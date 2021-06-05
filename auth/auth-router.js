551e6b042a6195af6ca7a74a2d5d8c086c8b526e
Heber <hebergonza@gmail.com>
2021-06-05 02:49:40 -0400

    finished project

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -41,7 +41,7 @@ router.post('/login', (req, res, next) => {
       // user.password come from database
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = generateToken(user);
-        res.status(200).json({ message: `Welcome ${user.username}!`, jwt_token: token, user_id: user.id });
+        res.status(200).json({ message: `Welcome ${user.username}!`, jwt_token: token, user_id: user.id, username: user.username });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
       }
