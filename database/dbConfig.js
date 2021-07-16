62efb03a1e7d7b5843aa96936da2af1ca212b2e6
Heber <hebergonza@gmail.com>
2021-07-16 01:30:39 -0400

    modified dbconfig

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -5,5 +5,7 @@ const knexConfig = require('../knexfile.js');
 // this is from .env
 const environment = process.env.DB_ENV || 'development';
 
+knex.migrate.latest([knexConfig[environment]]);
+
 // exporting knex instance with knex config
 module.exports = knex(knexConfig[environment]);
