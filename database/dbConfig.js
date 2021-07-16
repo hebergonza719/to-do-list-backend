7cb53f78c510c75c5686c39d124b1e95294abec2
Heber <hebergonza@gmail.com>
2021-07-16 00:59:47 -0400

    changed env to production

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -3,7 +3,7 @@ const knex = require('knex');
 const knexConfig = require('../knexfile.js');
 
 // this is from .env
-const environment = process.env.DB_ENV || 'development';
+const environment = process.env.DB_ENV || 'production';
 
 // exporting knex instance with knex config
 module.exports = knex(knexConfig[environment]);
